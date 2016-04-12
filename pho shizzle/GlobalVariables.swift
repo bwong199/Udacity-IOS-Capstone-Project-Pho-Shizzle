da85640cd75bc5e79e203770ea8fca4bf138fa9b
bwong199 <bwong199@my.bcit.ca>
2016-04-11 23:20:25 -0600

    First Commit

---
+++ pho shizzle/GlobalVariables.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalVariables.swift
+//  pho shizzle
+//
+//  Created by Ben Wong on 2016-04-11.
+//  Copyright © 2016 Ben Wong. All rights reserved.
+//
+
+import UIKit
+
+class GlobalVariables {
+    static var phoInfoList : [Pho] = []
+}
---
+++ pho shizzle/Pho.swift
@@ -0,0 +1,23 @@
+//
+//  Pho.swift
+//  pho shizzle
+//
+//  Created by Ben Wong on 2016-04-11.
+//  Copyright © 2016 Ben Wong. All rights reserved.
+//
+
+import UIKit
+
+class Pho {
+    var name = ""
+    var address = ""
+    var postalCode = ""
+    var phoneNumber = 0
+    var rating = ""
+    var votes = ""
+    var distanceFromUser = 0
+    
+    init(){
+        
+    }
+}
